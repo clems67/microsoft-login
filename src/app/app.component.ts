72c4cc7f1b041061d4617bbb01db2a186c9083e2
CARAVITA <clement.caravita@sogeti.com>
2023-03-29 15:37:16 +0200

    MSAL setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { MsalService } from '@azure/msal-angular/msal.service';
+import { AuthenticationResult } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'microsoft-login';
+
+  constructor(private msalService: MsalService) {}
+
+  isLoggedIn(): boolean {
+    return this.msalService.instance.getActiveAccount() != null;
+  }
+
+  login() {
+    this.msalService
+      .loginPopup()
+      .subscribe((response: AuthenticationResult) => {
+        this.msalService.instance.setActiveAccount(response.account);
+      });
+  }
+
+  logout() {
+    this.msalService.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MsalModule } from '@azure/msal-angular';
+import { MSAL_INSTANCE } from '@azure/msal-angular/constants';
+import { MsalService } from '@azure/msal-angular/msal.service';
+import { PublicClientApplication } from '@azure/msal-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+export function MSALInstanceFactory(): PublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '023d108a-403c-4200-9470-3f9da3550c0f',
+      redirectUri: 'http://localhost:4200',
+    },
+  });
+}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, MsalModule],
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+    MsalService,
+  ],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
